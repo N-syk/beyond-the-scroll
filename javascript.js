26907e103ca9492cdcfb3474b93758cd513efcb5
Sayaka Nakayama <n_syk_a0409@icloud.com>
2020-08-03 00:00:44 +0900

    むずい

--- javascript.js
+++ javascript.js
@@ -1,31 +1,35 @@
 $(function(){
     //div追加
-    var html = "";
+    var html1 = "";
     var html2 = "";
+    var html3 = "";
     for (var i=1; i<=10; i++) {
-        html += "<div class='box'></div>";
+        html1 += "<div class='box'></div>";
         html2 += "<div class='box2'></div>";
+        html3 += "<div class='box3'></div>";
     }
-    $('h1').after(html);
-    $('h1').after(html2);
+    $('h1').after(html1, html2, html3);
 
     //boxクラスにid付与
     $('.box').each(function(i){
-        $(this).attr("id", "box"+(i+1));
+        $(this).attr("id", "box"+(1+i));
     });
     $('.box2').each(function(i){
         $(this).attr("id", "box"+(11+i));
     });
+    $('.box3').each(function(i){
+        $(this).attr("id", "box"+(21+i));
+    });
     
-    //パララックス設定
-   var top = $('#box1').offset().top;
-   var bottom = $('#box11').offset().bottom;//bottomは取得できない
+   //var windowHeight = $(window).height();
+   var top = $('#box1').position().top;
+   var bottom = 5000 - $('#box11').position().top;//※bottomは取得できない
    var clockOffset = $('.whole_clock').offset().top;
     $(window).scroll(function(){
         var value = $(this).scrollTop();
         $('#scrollValue').text(value);
         $('#clock').text(20+Math.floor(value/900));
-        
+        //時計がついてくる
         if(value > clockOffset - 30){
             $('.whole_clock').css({
                 'position':'fixed',
@@ -34,12 +38,15 @@ $(function(){
         }else{
             $('.whole_clock').css('position','static');
         }
-
+        //ぴよこが動く
         $('.box').each(function(i){
-            $(this).css('top', top + value / (i+1));
+            $(this).css('top', top + value);
         });
         $('.box2').each(function(i){
-            $(this).css('bottom', bottom + value / (i+1));
+            $(this).css('bottom', bottom + value);
+        });
+        $('.box3').each(function(i){
+            $(this).css('bottom', bottom + value + 10);
         });
     });
 });

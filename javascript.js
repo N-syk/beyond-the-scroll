3a7be7814d6d5d4d68e946af6745bfcb90c5a093
Sayaka Nakayama <n_syk_a0409@icloud.com>
2020-08-04 21:00:45 +0900

    地道な作業だ、がんばれ

--- javascript.js
+++ javascript.js
@@ -3,31 +3,51 @@ $(function(){
     var html1 = "", html2 = "", html3 = "", html4 = "";
     for (var i=1; i<=25; i++) {
         html1 += "<div class='box'><a class='clickToolTip'></a></div>";
-        html2 += "<div class='box2'></div>";
-        html3 += "<div class='box3'></div>";
-        html4 += "<div class='box4'></div>";
+        html2 += "<div class='box2'><a class='clickToolTip'></a></div>";
+        html3 += "<div class='box3'><a class='clickToolTip'></a></div>";
+        html4 += "<div class='box4'><a class='clickToolTip'></a></div>";
     }
     $('h1').after(html1, html2, html3, html4);
 
+    //p追加
+    var html5 = "";
+    for(var i=1; i<=100; i++){
+        html5 += "<p class='toolTip invisible'></p>";
+    }
+    $('.whole_clock').after(html5);
+    $('.toolTip').each(function(i){
+        $(this).attr("id", "comment"+(1+i));
+    });
+
+    
+
     //boxクラスにid付与
+    //初期位置の取得
+    var posiTop1 = [], posiTop2 = [];
+    var posiBottom1 = [], posiBottom2 = [];
     $('.box').each(function(i){
         $(this).attr("id", "box"+(1+i),);
+       posiTop1.push($('#box'+(1+i)).position().top);
     });
+    $('.box2').each(function(i){
+        $(this).attr("id", "box"+(26+i));
+        posiBottom1.push(3400 - $('#box'+(26+i)).position().top);
+    });
+    $('.box3').each(function(i){
+        $(this).attr("id", "box"+(51+i));
+        posiTop2.push($('#box'+(51+i)).position().top);
+    });
+    $('.box4').each(function(i){
+        $(this).attr("id", "box"+(76+i));
+        posiBottom2.push(3400 - $('#box'+(76+i)).position().top);
+    });
+
+    //コメントにid付与
     $('.clickToolTip').each(function(i){
         $(this).attr("href", "#comment"+(1+i));
     });
-    $('.box2').each(function(i){
-        $(this).attr("id", "box"+(11+i));
-    });
-    $('.box3').each(function(i){
-        $(this).attr("id", "box"+(21+i));
-    });
-    $('.box4').each(function(i){
-        $(this).attr("id", "box"+(31+i));
-    });
     
-   var top = $('#box1').position().top;
-   var bottom = 3000 - $('#box26').position().top;//※bottomは取得できない
+    //スクロールしたら…
    var clockOffset = $('.whole_clock').offset().top;
     $(window).scroll(function(){
         var value = $(this).scrollTop();
@@ -43,31 +63,29 @@ $(function(){
             $('.whole_clock').css('position','static');
         }
         //ぴよこが動く
-        var top_walk = value　* 0.8;
-        var bottom_walk = bottom + value;
-        var top_run = value * 1.2;
-        var bottom_run = bottom + value * 1.24;
+        var walk = value　* 0.8;
+        var run = value * 1.2;
         $('.box').each(function(i){
-            if(top_walk < 3000){
+            if(walk < 3400){
                 $(this).show();
-                $(this).css('top',top + top_walk);
+                $(this).css('top', posiTop1[i] + walk);
             }else{
                 $(this).hide();
             }
         });
         $('.box2').each(function(i){
-            $(this).css('bottom', bottom_walk);
+            $(this).css('bottom', posiBottom1[i] + walk);
         });
         $('.box3').each(function(i){
-            if(10 + top_run < 3000){
+            if(10 + run < 3400){
                 $(this).show();
-                $(this).css('top', top + 10 + top_run);
+                $(this).css('top', posiTop2[i] + run);
             }else{
                 $(this).hide();
             }
         });
         $('.box4').each(function(i){
-            $(this).css('bottom', 10 + bottom_run);
+            $(this).css('bottom', 10 + posiBottom2[i] + run);
         });
 
         //非表示コメントの位置
@@ -87,6 +105,7 @@ $(function(){
         $('p'+targetNote).removeClass('invisible');
         return false;
     });
+    //クリックでコメント非表示
     $('html').mousedown(function(){
         $('p.toolTip').addClass('invisible');
         $('a.clickToolTip').prop("disabled", false);       
